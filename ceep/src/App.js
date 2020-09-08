533c3b03ed701772b108071dc2bd618e0cd5019d
maulru <maurojr_@outlook.com>
2020-09-08 20:55:19 -0300

    Aulas 1 e 2 - Componentizando o sistema

---
+++ ceep/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/listaDeNotas";
+import FormularioCadastro from "./components/formularioCadastro";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
+}
+
+export default App;
