d969189e665e31a605c048db8ba243c00d8facb7
maulru <maurojr_@outlook.com>
2020-09-11 21:24:48 -0300

    Aula 05 - Alterando estado e renderização

--- ceep/src/components/FormularioCadastro/formularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/formularioCadastro.jsx
@@ -1,30 +1,43 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.titulo = "";
+    this.texto = "";
   }
 
-  handleMudancaTitulo(evento) {
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
     this.titulo = evento.target.value;
-    console.log(evento.target.value);
+  }
+
+  _handleMudancaTexto(evento) {
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
   }
 
   render() {
     return (
-      <form className="form-cadastro">
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudancaTitulo.bind(this)}
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
 
         <textarea
           className="form-cadastro_input"
           placeholder="Escreva sua nota..."
           rows={15}
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Inserir Nota
